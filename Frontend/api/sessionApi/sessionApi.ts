1d3417a70ce81dd23ce05b75898c4f983f169884
Niall_Hogan <b9013287@my.shu.ac.uk>
2022-11-14 13:04:01 +0000

    feat: extracted API to config

--- Frontend/api/sessionApi/sessionApi.ts
+++ Frontend/api/sessionApi/sessionApi.ts
@@ -1,18 +1,13 @@
-import React from 'react'
-import axios from 'axios'
-import { ISession } from '../../types/types'
-
-
-const instance = axios.create({
-    baseURL: 'http://localhost:8080/',
-})
+import React from "react";
+import api from "../config/index";
+import { ISession } from "../../types/types";
 
 export const newSessionApi = async (session: ISession) => {
-    const res = await instance.post<ISession>('session', session, {
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-      },)
-    console.log(res);
-}
+  const res = await api.post<ISession>("session", session, {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+  console.log(res);
+};
