261540f8da3e48a6431838498d37ae9da1fe525f
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2022-11-16 15:36:24 +0000

    updated sessionController

--- Backend/src/Models/Session.ts
+++ Backend/src/Models/Session.ts
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import { ISession } from "../Interfaces/ISession";
 
-export const sessionSchema = new Schema<ISession>({
+const sessionSchema = new Schema<ISession>({
   sessionName: { type: String, required: true },
   date: { type: String, required: true },
 });
