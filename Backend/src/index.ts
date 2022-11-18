a667cc4d581b545d75211aa208ec888caa243f6a
Girts Jermacans <girts.jermacans@gmail.com>
2022-11-18 16:43:47 +0000

    Split RouteHandler into multiple so now can add JWT to secure routes

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
-import RouteHandler from "./Routes/RouteHandler";
+import UserRouteHandler from "./Routes/UserRouteHandler";
+import SessionRouteHandler from "./Routes/SessionRouteHandler";
 import { accessToken } from "./middleware/jwt"
+import StudentRouteHandler from "./Routes/StudentRouteHandler";
 
 dotenv.config();
 
@@ -37,4 +39,6 @@ process.on("SIGINT", () => {
   });
 });
 
-app.use("/", RouteHandler);
+app.use("/user", UserRouteHandler);
+app.use("/session", SessionRouteHandler);
+app.use("/student", StudentRouteHandler)
