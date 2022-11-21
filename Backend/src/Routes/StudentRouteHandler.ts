564db281779f7bd2830cb6656f21151062149c15
Girts Jermacans <girts.jermacans@gmail.com>
2022-11-21 12:25:20 +0000

    added middleware that can be used to protect backend routes

--- Backend/src/Routes/StudentRouteHandler.ts
+++ Backend/src/Routes/StudentRouteHandler.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import { checkAuth } from "../middleware/CheckAuth";
 import studentController from "../Controllers/StudentController";
 
 const StudentRouteHandler = express.Router();
 
+// StudentRouteHandler.use(checkAuth);
 StudentRouteHandler.use(studentController);
 
 export default StudentRouteHandler;
