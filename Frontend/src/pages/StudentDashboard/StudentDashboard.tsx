e3e7f581b057856c0f1d74116b962c57f1e25d0d
Niall_Hogan <b9013287@my.shu.ac.uk>
2022-11-21 17:24:56 +0000

    feat: added toasts

--- Frontend/src/pages/StudentDashboard/StudentDashboard.tsx
+++ Frontend/src/pages/StudentDashboard/StudentDashboard.tsx
@@ -1,15 +1,48 @@
 import { Button, Flex, Input, FormLabel, FormControl } from "@chakra-ui/react";
 import { useStore } from "../../contexts/storeProvider";
-
 import { PageWithSideBar } from "../../components/PageWithSideBar/PageWithSideBar";
 import { DynamicNavBar } from "../../components/DynamicNavbar/DynamicNavBar";
 import { useMutation } from "react-query";
 import { useState } from "react";
+import { useToast } from "@chakra-ui/react";
 
 const StudentDashboard = () => {
   const store = useStore();
+  const toast = useToast();
 
   const [sessionCode, setSessionCode] = useState<string>("");
+
+  const handleUseMutation = async (sessionCode: string) => {
+    console.log("sessionCode ==> ", sessionCode);
+    // handle api in here
+  };
+
+  const mutation = useMutation({
+    mutationFn: handleUseMutation,
+  });
+
+  const handleSubmit = () => {
+    mutation.mutate(sessionCode, {
+      onSuccess: (response) => {
+        toast({
+          title: "Joined Session",
+          description: `You have successfully joined session ${sessionCode}`,
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        });
+      },
+      onError: (error) => {
+        toast({
+          title: "Error Joining Session",
+          description: `Cannot join session ${sessionCode}`,
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      },
+    });
+  };
 
   return (
     <PageWithSideBar
@@ -37,6 +70,7 @@ const StudentDashboard = () => {
               background="#17BEBB"
               _hover={{ bg: "#58edea" }}
               isDisabled={sessionCode.length < 6}
+              onClick={handleSubmit}
             >
               Submit
             </Button>
