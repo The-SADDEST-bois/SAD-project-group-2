9c79d7d9902d0452d5278a781855be49caa89089
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2022-11-21 14:39:57 +0000

    await user fix

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,7 +4,6 @@ import Home from "./pages/home/Home";
 import { RotatingLines } from "react-loader-spinner";
 import { useStore } from "./contexts/storeProvider";
 import { IUser } from "../types/types";
-import ProtectedRoutes from "./components/ProtectedRoutes";
 import { Roles } from "../types/roles";
 import CustomError from "./components/CustomError";
 const TestUseQuery = lazy(() => import("./pages/testusequery/TestUseQuery"));
@@ -16,10 +15,86 @@ const App = () => {
   const authStore = useStore();
 
   const [currentUser, setCurrentUser] = useState<IUser | null>(null);
+  
+  const [routes, setRoutes] = useState<JSX.Element>();
 
   useEffect(() => {
     setCurrentUser(authStore.auth.user);
+
+    if (currentUser === null) {
+      setRoutes(
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<RegisterNewUser />} />
+          <Route path="*" element={<CustomError errorMessage='first'/>} />
+        </Routes>
+      );
+    } else {
+    switch (currentUser?.role) {
+      case Roles.Admin:
+        setRoutes(
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<RegisterNewUser />} />
+            <Route path="/test" element={<TestUseQuery />} />
+            <Route path="/newsession" element={<NewSession />} />
+            <Route path="*" element={<CustomError errorMessage='second'/>} />
+          </Routes>
+        );
+      case Roles.CourseLeader:
+        setRoutes(
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<RegisterNewUser />} />
+            <Route path="/test" element={<TestUseQuery />} />
+            <Route path="/newsession" element={<NewSession />} />
+            <Route path="*" element={<CustomError errorMessage='third'/>} />
+          </Routes>
+        );
+      case Roles.ModuleLeader:
+        setRoutes(
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<RegisterNewUser />} />
+            <Route path="/test" element={<TestUseQuery />} />
+            <Route path="/newsession" element={<NewSession />} />
+            <Route path="*" element={<CustomError errorMessage='fourth'/>} />
+          </Routes>
+        );
+      case Roles.AcademicAdvisor:
+        setRoutes(
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<RegisterNewUser />} />
+            <Route path="/test" element={<TestUseQuery />} />
+            <Route path="/newsession" element={<NewSession />} />
+            <Route path="*" element={<CustomError errorMessage='fifth'/>} />
+          </Routes>
+        );
+      case Roles.Tutor:
+        setRoutes(
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<RegisterNewUser />} />
+            <Route path="/test" element={<TestUseQuery />} />
+            <Route path="/newsession" element={<NewSession />} />
+            <Route path="*" element={<CustomError errorMessage='sixth'/>} />
+          </Routes>
+        );
+      case Roles.Student:
+        setRoutes(
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<RegisterNewUser />} />
+            <Route path="/test" element={<TestUseQuery />} />
+            <Route path="/newsession" element={<NewSession />} />
+            <Route path="*" element={<CustomError errorMessage='seventh'/>} />
+          </Routes>
+        );
+    }
+    }
   }, [authStore.auth.user]);
+
 
   return (
     <Suspense
@@ -33,15 +108,7 @@ const App = () => {
         />
       }
     >
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<RegisterNewUser />} />
-        <Route path="/TestUseQuery" element={<TestUseQuery />} />
-        <Route element={<ProtectedRoutes user={currentUser} role={Roles.Tutor}/>}>
-          <Route path="/NewSession" element={<NewSession />} />
-        </Route>
-        <Route path="*" element={<CustomError errorMessage="404 Not Found" />}/>
-      </Routes>
+        {routes}
     </Suspense>
   );
 };
