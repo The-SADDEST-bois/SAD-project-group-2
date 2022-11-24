c82a8d0d521c083577814432c234da8df289d6af
chrisF20 <b9015280@my.shu.ac.uk>
2022-11-24 12:35:59 +0000

    cypress to jest

---
+++ Frontend/Test/verify.test.ts
@@ -0,0 +1,10 @@
+// import 
+
+// describe("test add function", () => {
+//     it("", () => {
+//       expect(add()).toBe();
+//     });
+//   it("", () => {
+//       expect(add()).toBe();
+//     });
+//   });
