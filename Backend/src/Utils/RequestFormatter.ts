5e273ff400ccb273dd2877215f03ef9e7319e236
James Ashby <60621690+james-ashby@users.noreply.github.com>
2022-11-25 15:17:54 +0000

    added CheckRole, RequestFormatter, added Role check to register attendance

---
+++ Backend/src/Utils/RequestFormatter.ts
@@ -0,0 +1,3 @@
+export const GetRoleFromRequest = (request: any) => {
+  return request.body.role;
+};
--- Backend/src/middleware/checkAuth.ts
+++ Backend/src/middleware/checkAuth.ts
@@ -1,25 +1,27 @@
+import { ITokenData } from "../Interfaces/ITokenData";
 import { verifyToken } from "../middleware/jwt";
 
 export const checkAuth = (request: any, response: any, next: any) => {
-
-    try {
-        const token = request.headers.authorization.split(" ")[1];
-        if (!token) {
-            console.log("Token is missing");
-            response.status(401).json({ error: "Missing token" }).send();
-        } else {
-            verifyToken(token)
-                .then(() => {
-                    console.log("Token verified");
-                    next();
-                })
-                .catch(() => {
-                    console.log("Bad token");
-                    response.status(401).json({ error: "Invalid token" }).send();
-                });
-        }
-    } catch (error) {
-        console.log("No authorization header set");
-        response.status(401).json({ error: "Bearer not added" }).send();
+  try {
+    const token = request.headers.authorization.split(" ")[1];
+    if (!token) {
+      console.log("Token is missing");
+      response.status(401).json({ error: "Missing token" }).send();
+    } else {
+      verifyToken(JSON.parse(token))
+        .then((data) => {
+          const tokenData = data as unknown as ITokenData;
+          console.log("Token verified");
+          request.body.role = tokenData.data.role;
+          next();
+        })
+        .catch(() => {
+          console.log("Bad token");
+          response.status(401).json({ error: "Invalid token" }).send();
+        });
     }
-}
+  } catch (error) {
+    console.log("No authorization header set");
+    response.status(401).json({ error: "Bearer not added" }).send();
+  }
+};
