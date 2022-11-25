6df7c3924fe22a6603f9444756b1c9c950b660f0
Girts Jermacans <girts.jermacans@gmail.com>
2022-11-25 14:53:40 +0000

    added new endpoint for getting sessions per module

--- Backend/src/Controllers/TutorController.ts
+++ Backend/src/Controllers/TutorController.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import StatusCode from "../Utils/StatusCodes";
 import Modules from "../Models/Module";
+import Sessions from "../Models/Session";
 
 const tutorController = express.Router();
 
@@ -18,5 +19,19 @@ tutorController.get("/allModules", (request: any, response: any) => {
         }
     });
   });
+
+tutorController.get("/sessionsPerModule", (request: any, response: any) => {
+    const moduleName = request.query.moduleName;
+    Sessions.find({ moduleName: moduleName}, (err: any, document: any) => {
+        if (err) {
+            console.log("Error finding sessions per module name", err);
+            response
+            .status(err.status || StatusCode.BAD_REQUEST)
+            .json({ error: "Error getting sessions", message: err });
+        } else {
+            response.status(StatusCode.OK).json(document);
+        }
+    });
+});
 
 export default tutorController;
