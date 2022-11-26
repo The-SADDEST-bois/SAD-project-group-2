11a0683b2e0563c9f55c70349015fb39a18321a4
James Ashby <60621690+james-ashby@users.noreply.github.com>
2022-11-26 13:56:39 +0000

    added role restriction to sessioncontroller

--- Backend/src/Controllers/SessionController.ts
+++ Backend/src/Controllers/SessionController.ts
@@ -2,6 +2,11 @@ import express from "express";
 import { ISession } from "../Interfaces/ISession";
 import StatusCode from "../Utils/StatusCodes";
 import Sessions from "../Models/Session";
+import {
+  IsCourseLeaderRole,
+  IsTutorRole,
+  IsModuleLeaderRole,
+} from "../Utils/CheckRole";
 const sessionController = express.Router();
 
 // Session controller post endpoint (adds session to database) (can rename to /createSession if necessary)
@@ -10,24 +15,38 @@ sessionController.post("/", (request, response) => {
   response.status(404).json({ message: "Not found" });
 });
 
-sessionController.post("/toggleSession", (request, response) => {
-  const body: ISession = request.body;
-  Sessions.findOneAndUpdate(
-    { _id: body._id },
-    { isOpen: body.isOpen },
-    { new: true },
-    (err, doc) => {
-      if (err) {
-        response
-          .status(StatusCode.INTERNAL_SERVER_ERROR)
-          .json({ message: "Internal server error" });
-      } else {
-        response
-          .status(StatusCode.OK)
-          .json({ message: "Session updated successfully" });
+sessionController.post("/toggleSession", (request: any, response: any) => {
+  if (
+    !IsCourseLeaderRole(request) ||
+    !IsModuleLeaderRole(request) ||
+    !IsTutorRole(request)
+  ) {
+    response
+      .status(StatusCode.FORBIDDEN)
+      .json({
+        error: "Forbidden",
+        message: "You are do not have the correct privileges for this request",
+      })
+      .send();
+  } else {
+    const body: ISession = request.body;
+    Sessions.findOneAndUpdate(
+      { _id: body._id },
+      { isOpen: body.isOpen },
+      { new: true },
+      (err, doc) => {
+        if (err) {
+          response
+            .status(StatusCode.INTERNAL_SERVER_ERROR)
+            .json({ message: "Internal server error" });
+        } else {
+          response
+            .status(StatusCode.OK)
+            .json({ message: "Session updated successfully" });
+        }
       }
-    }
-  );
+    );
+  }
 });
 
 sessionController.get("/allSessions", (request, response) => {
@@ -80,12 +99,10 @@ sessionController.post("/newSession", (request, response) => {
   const session = request.body;
   Sessions.create(session, (err: any, document: any) => {
     if (err) {
-      console.log("error creating session", err);
       response
         .status(StatusCode.INTERNAL_SERVER_ERROR)
         .json({ error: "Error creating session", message: err });
     } else {
-      console.log("successful session creation", document);
       response
         .status(StatusCode.OK)
         .json({ message: "Session created successfully" });
@@ -93,36 +110,54 @@ sessionController.post("/newSession", (request, response) => {
   });
 });
 
-sessionController.post("/sessionAttendance", async (request, response) => {
-  const body = request.body as {
-    sessionId: string;
-    firstName: string;
-    lastName: string;
-    status: number;
-    sessionCode: string;
-  };
-  const sessionCode = body.sessionCode;
+sessionController.post(
+  "/sessionAttendance",
+  async (request: any, response: any) => {
+    if (
+      !IsCourseLeaderRole(request) ||
+      !IsModuleLeaderRole(request) ||
+      !IsTutorRole(request)
+    ) {
+      response
+        .status(StatusCode.FORBIDDEN)
+        .json({
+          error: "Forbidden",
+          message:
+            "You are do not have the correct privileges for this request",
+        })
+        .send();
+    } else {
+      const body = request.body as {
+        sessionId: string;
+        firstName: string;
+        lastName: string;
+        status: number;
+        sessionCode: string;
+      };
+      const sessionCode = body.sessionCode;
 
-  Sessions.findOneAndUpdate(
-    {
-      sessionCode: sessionCode,
-    },
-    {
-      $set: { "attendance.$[v1].status": body.status },
-    },
-    {
-      arrayFilters: [{ "v1.firstName": body.firstName }],
-    },
-    (err: any, doc: any) => {
-      if (err) {
-        console.log(err);
-        response.status(500).json({ message: "Internal server error" });
-      } else {
-        console.log("document = ", doc);
-        response.status(200).json({ message: "Success" });
-      }
+      Sessions.findOneAndUpdate(
+        {
+          sessionCode: sessionCode,
+        },
+        {
+          $set: { "attendance.$[v1].status": body.status },
+        },
+        {
+          arrayFilters: [{ "v1.firstName": body.firstName }],
+        },
+        (err: any, doc: any) => {
+          if (err) {
+            console.log(err);
+            response.status(500).json({ message: "Internal server error" });
+          } else {
+            console.log("document = ", doc);
+            response.status(200).json({ message: "Success" });
+          }
+        }
+      );
     }
-  );
-});
+  }
+);
 
 export default sessionController;
