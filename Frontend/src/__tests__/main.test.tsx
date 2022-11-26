1acd91587133cc6bdb3c788752b18217d647af74
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2022-11-26 17:29:00 +0000

    jest config and setup for react

---
+++ Frontend/src/__tests__/main.test.tsx
@@ -0,0 +1,18 @@
+// Imports
+import { render } from '@testing-library/react';
+
+// To Test
+import Login from '../pages/login/login';
+
+// Tests
+test('Renders main page correctly', () => {
+// Setup
+render(<Login />);
+
+// Pre Expecations
+
+// Init
+
+// Post Expectations
+  expect(true).toBeTruthy();
+});
