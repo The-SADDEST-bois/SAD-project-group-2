3473f03355979221b4934f6d142a756606921914
James Ashby <60621690+james-ashby@users.noreply.github.com>
2022-11-28 13:02:37 +0000

    added overallmoduleattendance endpoint as percentage

--- Frontend/api/tutorApi/tutorApi.ts
+++ Frontend/api/tutorApi/tutorApi.ts
@@ -11,8 +11,8 @@ export const getAllModulesByTutor = async (tutorId: string) => {
   try {
     return await api.get<IModule[]>(
       "/tutor/allModules",
-      headerAuthorisationWithParams({tutorId: tutorId})
-    ); 
+      headerAuthorisationWithParams({ tutorId: tutorId })
+    );
   } catch (error: any) {
     console.log(error);
     return error.response;
@@ -23,11 +23,22 @@ export const getAllSessionsPerModule = async (moduleName: string) => {
   try {
     return await api.get<ISession[]>(
       "/tutor/sessionsPerModule",
-      headerAuthorisationWithParams({moduleName: moduleName})
+      headerAuthorisationWithParams({ moduleName: moduleName })
     );
   } catch (error: any) {
     console.log(error);
     return error.response;
   }
+};
 
+export const getOverallModuleAttendance = async (moduleName: string) => {
+  try {
+    return await api.get<ISession[]>(
+      "/tutor/overallModuleAttendance",
+      headerAuthorisationWithParams({ moduleName: moduleName })
+    );
+  } catch (error: any) {
+    console.log(error);
+    return error.response;
+  }
 };
