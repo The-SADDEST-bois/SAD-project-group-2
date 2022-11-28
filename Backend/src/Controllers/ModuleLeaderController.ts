e2707082429ed11d671ae06d89962228ccabcebc
James Ashby <60621690+james-ashby@users.noreply.github.com>
2022-11-28 13:45:43 +0000

    added delete session and cohort attendance

--- Backend/src/Controllers/ModuleLeaderController.ts
+++ Backend/src/Controllers/ModuleLeaderController.ts
@@ -96,4 +96,25 @@ moduleLeaderController.get(
   }
 );
 
+moduleLeaderController.delete(
+  "/deleteSession",
+  (request: any, response: any) => {
+    if (!IsModuleLeaderRole(request)) {
+      return response.status(StatusCode.FORBIDDEN).json({
+        error: "Forbidden",
+        message: "You are do not have the correct privileges for this request",
+      });
+    }
+    const sessionId = request.query.sessionId;
+    Sessions.deleteOne({ _id: sessionId }, (err: any, document: any) => {
+      if (err) {
+        return response
+          .status(err.status || StatusCode.BAD_REQUEST)
+          .json({ error: "Error deleting session", message: err });
+      }
+      return response.status(StatusCode.OK).json(document);
+    });
+  }
+);
+
 export default moduleLeaderController;
