a684fb9d02d5739d9e994a39c7dabb445163b475
Girts Jermacans <girts.jermacans@gmail.com>
2022-11-29 01:32:14 +0000

    added course leader business component

--- Backend/src/Controllers/CourseLeaderController.ts
+++ Backend/src/Controllers/CourseLeaderController.ts
@@ -1,8 +1,7 @@
 import express from "express";
 import StatusCode from "../Utils/StatusCodes";
 import { IsCourseLeaderRole } from "../Utils/CheckRole";
-import Courses from "../Models/Course";
-import Sessions from "../Models/Session";
+import { CoursesByCourseLeader, OverallCourseAttendance } from "../Services/CourseLeaderServices";
 
 const courseLeaderController = express.Router();
 
@@ -13,68 +12,21 @@ courseLeaderController.get("/allCourses", (request: any, response: any) => {
       message: "You do not have the correct privileges for this request",
     });
   }
-  const courseLeaderId = request.query.courseLeaderId;
-  Courses.find(
-    { "courseLeader._id": courseLeaderId },
-    (err: any, document: any) => {
-      if (err) {
-        console.log("Error getting courses: ", err);
-        return response
-          .status(err.status || StatusCode.BAD_REQUEST)
-          .json({ error: "Error getting courses", message: err });
-      }
-      return response.status(StatusCode.OK).json(document);
-    }
-  );
+
+  return CoursesByCourseLeader(request, response);
+
 });
 
-courseLeaderController.get(
-  "/overallCourseAttendance",
-  async (request: any, response: any) => {
+courseLeaderController.get("/overallCourseAttendance", async (request: any, response: any) => {
     if (!IsCourseLeaderRole(request)) {
       return response.status(StatusCode.FORBIDDEN).json({
         error: "Forbidden",
         message: "You are do not have the correct privileges for this request",
       });
     }
-    try {
-      const courseName = request.query.courseName;
-      const attendanceFromAllSessionsInCourse = await Sessions.find({
-        courseName: courseName,
-      }).select("attendance");
 
-      var numberOfStudents = 0;
-      for (let i = 0; i < attendanceFromAllSessionsInCourse.length; i++) {
-        numberOfStudents +=
-          attendanceFromAllSessionsInCourse[i].attendance.length;
-      }
+    return await OverallCourseAttendance(request, response);
 
-      var numberOfAttended = 0;
-      for (let i = 0; i < attendanceFromAllSessionsInCourse.length; i++) {
-        attendanceFromAllSessionsInCourse[i].attendance.forEach(
-          (student: any) => {
-            if (student.status === 1) {
-              numberOfAttended++;
-            }
-          }
-        );
-      }
-
-      const overallAttendancePercentage = (
-        (numberOfAttended / numberOfStudents) *
-        100
-      ).toFixed(2);
-
-      return response.status(StatusCode.OK).json({
-        overallAttendancePercentage: overallAttendancePercentage,
-      });
-    } catch (error) {
-      return response.status(StatusCode.INTERNAL_SERVER_ERROR).json({
-        error: "Internal Server Error",
-        message: "Error getting overall course attendance",
-      });
-    }
-  }
-);
+  });
 
 export default courseLeaderController;
