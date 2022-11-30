f07829700475a2f74c5c65fd8921a1efa56276e7
Corey Sutcliffe <Corey.j.sutcliffe@gmail.com>
2022-11-30 00:21:56 +0000

    cypress fix.........

--- Frontend/cypress/cypress.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
-  waitForAnimations: false,
-  animationDistanceThreshold: 50
-})
-
-// script to run cypress not sure is needed "cy:run": "cypress run --browser chrome --config-file cypress/cypress.config.ts",
--- Frontend/cypress/e2e/homepage.cy.ts
+++
@@ -1,7 +0,0 @@
-import type * as jest from 'jest';
-
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('http://127.0.0.1:5173/')
-  })
-})
